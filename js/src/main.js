f48d7b74ce5989e38a087ff6628a143d2faeaa19
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-07-18 16:11:24 +0300

    Created function, which draws the playing field

--- js/src/main.js
+++ js/src/main.js
@@ -1,10 +1,21 @@
 const canvas = document.getElementById('game');
-const ctx = canvas.getContext("sd"); // устанавливаю формат игры
+const ctx = canvas.getContext("2d"); // устанавливаю формат игры
 
 const ground = new Image(); //специальный класс, который позволяет работать с картинками
-ground.src = "images/ground.png";
+ground.src = "/img/ground.png";
 const food = new Image();
-ground.src = "images/carrot.png";
+food.src = "/img/carrot.png";
 
 let box = 32; //переменная отвечающая за размеры одной ячейки на поле для игры
 let score = 0; //переменная содержащая колличество очков
+
+
+// функция, которая рисует объекты внутри canvas
+function drawGame() {
+    //функция canvasб которая позволяет рисовать картинки
+    ctx.drawImage(ground, 0, 0); //указываю картинку с которой буду работать и координаты x, y
+};
+
+// помещаем функция для отрисовки, котораядолжна выполняться каждые 100 млсек.
+let game = setInterval(drawGame, 100);
+
